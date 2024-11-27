24ae9f4826a01a9c711fe1bab3e2d6b2752ce352
tihomir.nikolov1988 <tihomir.nikolov1988@gmail.com>
2024-11-27 13:41:22 +0200

    update work links

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -27,6 +27,9 @@ const workSlides = {
         {
           title: 'Personal website',
           path: '/Thumb-personal-website.png',
+          github: 'https://github.com/Tihomir-N/dev-profile',
+          isLive: true,
+          website: 'https://tihomir-dev-profile.web.app/'
         },
       ],
     },
@@ -79,59 +82,61 @@ const WorkSlider = () => {
       modules={[Pagination]}
       className="h-[280px] sm:h-[480px]"
     >
-{workSlides.slides.map((slide, index) => {
-  return (
-    <SwiperSlide key={index}>
-      <div className='grid grid-cols-2 gap-rows-2 gap-4 cursor-pointer'>
-        {slide.images.map((image, i) => {
-          return (
-            <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={i}>
-              <div className='flex items-center justify-center relative overflow-hidden group'>
-                {/* image */}
-                <Image src={image.path} width={500} height={300} alt="" />
-                {/* overlay gradient */}
-                <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
-                {/* title and icons */}
-                <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 flex flex-col items-center justify-center'>
-                  <div className='flex flex-col items-center gap-y-2 text-[13px] tracking-[0.2em]'>
-                    <div className="text-base font-medium font-sora mb-2 md:mb-0">{image.title}</div>
-                    {/* title and arrow */}
-                    <div className='flex items-center gap-x-2'>
-                      {image.isLive && <div className='delay-100'>LIVE</div>}
-                      <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
-                      <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
-                        <BsArrowRight />
+      {workSlides.slides.map((slide, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className='grid grid-cols-2 gap-rows-2 gap-4 cursor-pointer'>
+              {slide.images.map((image, i) => {
+                return (
+                  <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={i}>
+                    <div className='flex items-center justify-center relative overflow-hidden group'>
+                      {/* image */}
+                      <Image src={image.path} width={500} height={300} alt="" />
+                      {/* overlay gradient */}
+                      <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+                      {/* title and icons */}
+                      <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300 flex flex-col items-center justify-center'>
+                        <div className='flex flex-col items-center gap-y-2 text-[13px] tracking-[0.2em]'>
+                          <a href={image.website || image.github} target="_blank" rel="noopener noreferrer">
+                            <div className="text-base font-medium font-sora mb-2 md:mb-0">{image.title}</div>
+                            {/* title and arrow */}
+                            <div className='flex items-center gap-x-2'>
+                              {image.isLive && <div className='delay-100'>LIVE</div>}
+                              <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
+                              <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'>
+                                <BsArrowRight />
+                              </div>
+                            </div>
+                          </a>
+                          {/* icons */}
+                          <div className='flex items-center gap-x-4 mt-2'>
+                            {image.github && <a
+                              href={image.github}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="text-2xl hover:text-accent transition-all duration-300"
+                            >
+                              <FaGithub />
+                            </a>}
+                            {image.website && <a
+                              href={image.website}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="text-2xl hover:text-accent transition-all duration-300"
+                            >
+                              <FaExternalLinkAlt />
+                            </a>}
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    {/* icons */}
-                    <div className='flex items-center gap-x-4 mt-2'>
-                      {image.github && <a
-                        href={image.github}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-2xl hover:text-accent transition-all duration-300"
-                      >
-                        <FaGithub />
-                      </a>}
-                      {image.website && <a
-                        href={image.website}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-2xl hover:text-accent transition-all duration-300"
-                      >
-                        <FaExternalLinkAlt />
-                      </a>}
-                    </div>
                   </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
-          );
-        })}
-      </div>
-    </SwiperSlide>
-  );
-})}
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
